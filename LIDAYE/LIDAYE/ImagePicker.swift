853fbe8a2bac6a4fbdbe482109f2050fa0b1b27b
LiDan <lidanlidan199@163.com>
2017-09-29 15:09:01 +0800

    creat

---
+++ LIDAYE/LIDAYE/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  LIDAYE
+//
+//  Created by 李丹 on 2017/9/29.
+//  Copyright © 2017年 李丹. All rights reserved.
+//
+
+import UIKit
+
+typealias didEndPlay = (() -> ())?
+
+class Test: NSObject {
+  public func testFunc() -> didEndPlay {
+    
+    return { }
+  }
+}
